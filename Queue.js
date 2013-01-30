5958710b5fa4614d4922b3d2b1201f9125ce7fc2
root <root@linux-fzdf.site>
2013-01-30 00:43:53 -0800

    QueueTest added, Queue modified to allow exporting

--- Queue.js
+++ Queue.js
@@ -11,49 +11,56 @@
 
         }
 
-        /**                                                                                                                                         
-        *                                                                                                                                         
-        * Retrieves the first object in the queue.                                                                                                   
-        *                                                                                                                                            
-        **/
-        Queue.prototype.dequeue = function(){
 
-            return this.arr.shift();
+        Queue.prototype = {
 
-        }
+	    /**
+	    *
+	    *
+	    * Retrieves the first object in the queue.
+	    *
+	    **/
+	    dequeue: function(){
+		
+		return this.arr.shift();
+		
+	    },
 
-       /**
-       *
-       * Puts the object onto the end of the queue.
-       *
-       * @param obj - The object to be added to the end of the queue
-       *
-       **/
-       Queue.prototype.enqueue = function(obj){
+	    /**
+	    *
+	    * Puts the object onto the end of the queue.
+	    *
+	    * @param obj - The object to be added to the end of the queue
+	    *
+	    **/
+	    enqueue: function(obj){
 
-	   this.arr.push(obj);
+		this.arr.push(obj);
 
-       }
+	    },
 
-      /*
-      *
-      * Retrieves the size of the Queue
-      *
-      **/
-      Queue.prototype.size = function(){
-	  
-	  return this.arr.length;
+	    /**
+	    *
+	    * Retrieves the size of the Queue.
+	    *
+	    **/
+	    size: function(){
 
-      }
+		return this.arr.length;
 
-      /*
-      *
-      * Clears all elements from the Queue
-      *
-      */
-      Queue.prototype.clear = function(){
+	    },
 
-	  this.arr = [];
+	    /**
+	    *
+	    * Clears all elements from the Queue
+	    *
+	    */
+	    clear: function(){
 
-      }
+		this.arr = [];
 
+	    }
+
+	}
+
+module.exports = new Queue();
